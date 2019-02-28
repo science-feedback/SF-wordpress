644509df8bddddd862e78a2adf16d57d7ca964d8
Manu <emvincent@nsup.org>
2019-02-28 11:44:38 +0100

    incl Clim rss

--- templates/head.php
+++ templates/head.php
@@ -5,20 +5,21 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="twitter:creator" content="@HealthFeedback">
+    <meta name="twitter:creator" content="@ScienceFeedback">
     
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
     <?php wp_head(); ?>
       
       <!-- GA -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-55053509-3"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-55053509-4"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-55053509-3');
+  gtag('config', 'UA-55053509-4');
 </script>
 
+      
   </head>
